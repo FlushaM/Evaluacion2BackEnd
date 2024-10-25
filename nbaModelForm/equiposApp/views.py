d118ac354a167aafb71709e29e85742890b56f46
FlushaM <83796763+FlushaM@users.noreply.github.com>
2024-10-25 20:37:11 -0300

    Update views.py
    .

--- nbaModelForm/equiposApp/views.py
+++ nbaModelForm/equiposApp/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import FormEquipo
+from .models import Equipo
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'Equipo/index.html')
+
+def actualizarEquipo(request, id):
+    proyectos = Equipo.objects.get(id = id)
+    form = FormEquipo(instance=proyectos)
+    if request.method == 'POST':
+        form = FormEquipo(request.POST, instance=proyectos)
+        if form.is_valid():
+            form.save()
+        return index(request)
+    data = {'form' : form}
+    return render(request, 'Equipo/agregarEquipo.html', data)
+
+
+def eliminarEquipo(request, id):
+    proyectos = Equipo.objects.get(id = id)
+    print(id)
+    proyectos.delete()
+    return redirect('../equipos/')
